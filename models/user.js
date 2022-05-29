f6783b5fa83de86fd05762fb0e4229fc8532fc8f
alpererdogan8 <alpererdogan8@gmail.com>
2022-05-29 12:21:02 +0300

    Add Mongo plugin

--- models/user.js
+++ models/user.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, minlength: 2 },
   password: { type: String, required: true, minlength: 12 },
 });
-
+UserSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model("User", UserSchema);
