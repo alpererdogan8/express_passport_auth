3a087aee6d097c8dba0f65b6c4666eaf77561aa8
alpererdogan8 <alpererdogan8@gmail.com>
2022-05-29 12:23:08 +0300

    codes have been reworked

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -1,36 +1,7 @@
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const bcrpyt = require("bcrypt");
-const user = require("../models/user");
+const passport = require("passport")
+const LocalStrategy = require("passport-local").Strategy
+const User = require("../models/user")
 
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function (user, done) {
-  done(null, user);
-});
-
-const option = (err, user, password, done) => {
-  if (err) {
-    return done(err);
-  }
-  if (!user) {
-    return done(null, false);
-  }
-
-  if (!bcrpyt.compareSync(password, user.password)) {
-    return done(null, false);
-  }
-  return done(null, user);
-};
-
-passport.use(
-  new LocalStrategy((username, password, done) => {
-    user.findOne({ username: username }, (err, user) =>
-      option(err, user, password, done)
-    );
-  })
-);
-
-module.exports = passport;
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
