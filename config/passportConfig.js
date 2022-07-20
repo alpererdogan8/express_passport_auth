42ea9b73ee34caa08243412c3045579d83e6fd22
Alper Erdogan <alpererdogan8@gmail.com>
2022-07-20 16:30:19 +0300

    deficiency has been corrected

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -5,3 +5,5 @@ const User = require("../models/user")
 passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
+
+module.exports = passport
