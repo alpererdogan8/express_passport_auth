57b421e741a55ee937450de0ac0ba3c878250545
alpererdogan8 <alpererdogan8@gmail.com>
2023-03-13 15:50:08 +0300

    passport field changed (passport-local-mongoose adds)

--- models/user.js
+++ models/user.js
@@ -1,9 +1,8 @@
-const mongoose = require("mongoose");
-const passportLocalMongoose = require("passport-local-mongoose");
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, minlength: 2 },
-  password: { type: String, required: true, minlength: 12 },
+	username: { type: String, required: true, minlength: 2 },
 });
 UserSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema);
