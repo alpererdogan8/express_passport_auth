4a0e2c5eb5e560108f9d0c0ca1ace37bad9b0768
alpererdogan8 <alpererdogan8@gmail.com>
2023-03-13 15:51:48 +0300

    auth controls reworked

--- router/auth.js
+++ router/auth.js
@@ -1,61 +1,61 @@
-const router = require("express").Router();
-const userService = require("../services/userService");
-const passportConfig = require("../config/passportConfig");
-
+const router = require('express').Router();
+const userService = require('../services/userService');
+const passportConfig = require('../config/passportConfig');
 
 router.use(passportConfig.initialize());
 router.use(passportConfig.session());
 
 const isAuth = (req, res, next) => {
-  if (!req.isAuthenticated()) return res.redirect("/");
-  return next();
+	if (!req.isAuthenticated()) return res.redirect('/');
+	return next();
 };
 
-router.get("/", isAuth, (req, res) => {
-  res.send({ status: "OK", path: "Auth" });
+router.get('/', isAuth, (req, res) => {
+	res.send({ status: 'OK', path: 'Auth' });
 });
 
-router.get("/user", isAuth, async (req, res) => {
-  const { username } = req.session.passport.user;
-  res.send(`Welcome ${username}`);
+router.get('/user', isAuth, async (req, res) => {
+	const { user } = req.session.passport;
+	res.send(`Welcome ${user}`);
 });
 
-router.get("/private", isAuth, (req, res) => {
-  res.send("Hey");
+router.get('/private', isAuth, (req, res) => {
+	res.send('Hey');
 });
-router.get("/private/:user", isAuth, async (req, res) => {
-  const { user } = req.params;
-  try {
-    const name = await userService.findUser(user);
-    res.send(`Welcome ${name.username}`);
-  } catch (error) {
-    res.status(404).send({ status: 404 });
-  }
+router.get('/private/:user', isAuth, async (req, res) => {
+	const { user } = req.params;
+	try {
+		const name = await userService.findUser(user);
+		res.send(`Welcome ${name.username}`);
+	} catch (error) {
+		res.status(404).send({ status: 404 });
+	}
 });
-router.get("/error", (req, res) => {
-  res.send("Wrong Login");
+router.get('/error', (req, res) => {
+	res.send('Wrong Login');
 });
 
-router.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-  const userFeedback = await userService.registerUser(username, password);
-  res.send(userFeedback);
+router.post('/register', async (req, res) => {
+	const { username, password } = req.body;
+
+	const userFeedback = await userService.registerUser(username, password);
+	res.send(userFeedback);
 });
 
 router.post(
-  "/login",
-  passportConfig.authenticate("local", {
-    successRedirect: "user",
-    failureRedirect: "error",
-  }),
-  function (req, res) {
-    res.redirect("/");
-  }
+	'/login',
+	passportConfig.authenticate('local', {
+		successRedirect: 'user',
+		failureRedirect: 'error',
+	}),
+	function (req, res) {
+		res.redirect('/');
+	},
 );
 
-router.post("/logout", (req, res) => {
-  req.logOut();
-  res.redirect("/");
+router.post('/logout', (req, res) => {
+	req.logOut();
+	res.redirect('/');
 });
 
 module.exports = router;
